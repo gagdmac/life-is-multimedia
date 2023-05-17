5b21f8db75b1287d618a440476e0900c42395b54
gab <garturogdiaz@gmail.com>
2023-05-17 12:09:13 +0200

    trasnlate and first component

---
+++ src/app/core/graphic-design-component/graphic-design-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graphic-design-component',
+  templateUrl: './graphic-design-component.component.html',
+  styleUrls: ['./graphic-design-component.component.scss']
+})
+export class GraphicDesignComponentComponent {
+
+}
---
+++ src/app/core/web-design-component/web-design-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web-design-component',
+  templateUrl: './web-design-component.component.html',
+  styleUrls: ['./web-design-component.component.scss']
+})
+export class WebDesignComponentComponent {
+
+}
