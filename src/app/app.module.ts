bfefc2b54d2e4ec6fa61f346b70d363eb455479a
gab <garturogdiaz@gmail.com>
2023-05-17 13:38:21 +0200

    menu - routing - styles - assets updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { SideMenuComponent } from "./core/side-menu/side-menu.component";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { GraphicDesignComponentComponent } from "./core/graphic-design-component/graphic-design-component.component";
-import { WebDesignComponentComponent } from "./core/web-design-component/web-design-component.component";
-import { AnimationComponentComponent } from "./core/animation-component/animation-component.component";
-import { VideoProductionComponentComponent } from "./core/video-production-component/video-production-component.component";
-import { UserExperienceDesignComponentComponent } from "./core/user-experience-design-component/user-experience-design-component.component";
+import { GraphicDesignComponentComponent } from "./feature/graphic-design-component/graphic-design-component.component";
+import { WebDesignComponentComponent } from "./feature/web-design-component/web-design-component.component";
+import { AnimationComponentComponent } from "./feature/animation-component/animation-component.component";
+import { VideoProductionComponentComponent } from "./feature/video-production-component/video-production-component.component";
+import { UserExperienceDesignComponentComponent } from "./feature/user-experience-design-component/user-experience-design-component.component";
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { UserExperienceDesignComponentComponent } from "./core/user-experience-d
     AnimationComponentComponent,
     VideoProductionComponentComponent,
     UserExperienceDesignComponentComponent,
+    SideMenuComponent,
   ],
   imports: [
     BrowserModule,
