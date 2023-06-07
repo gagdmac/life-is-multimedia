48e61956c580455bb69fe772f34cbd8c595a0864
gab <garturogdiaz@gmail.com>
2023-06-07 13:48:32 +0200

    Timeline and defaults

--- src/app/core/gsap-examples/gsap-examples.component.ts
+++ src/app/core/gsap-examples/gsap-examples.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit, ElementRef, AfterViewInit } from "@angular/core";
 import { gsap } from "gsap";
 
+let tl = gsap.timeline({
+  defaults: {
+    duration: 0.5,
+    ease: "power3.inOut",
+  },
+});
+
+gsap.defaults({
+  duration: 1, // Set global default duration to 1 second
+});
+
 @Component({
   selector: "app-gsap-examples",
   templateUrl: "./gsap-examples.component.html",
@@ -67,7 +78,6 @@ export class GsapExamplesComponent implements OnInit {
     });
 
     //STAGGERS
-
     const boxA = document.querySelectorAll(".boxA");
     gsap.to(boxA, {
       duration: 1,
@@ -96,22 +106,51 @@ export class GsapExamplesComponent implements OnInit {
       },
     });
 
+    // out of ngOnInit - just call
     gsap.set(".letter", { opacity: 0 });
+    gsap.set(".boxC1", {});
   }
+
   ngAfterViewInit() {
     const letters = this.elementRef.nativeElement.querySelectorAll(".letter");
-
     gsap.to(letters, {
       opacity: 1,
-      duration: 3,
+      duration: 1,
       repeat: -1,
+      y: 24,
       yoyo: true,
+      ease: "elastic.out(1, 0.3)",
       repeatDelay: 4,
       delay: 1,
       stagger: {
-        each: 0.5,
+        each: 0.2,
         from: "random",
       },
+    });
+
+    const boxC1 = this.elementRef.nativeElement.querySelector(".boxC1");
+    const boxC2 = this.elementRef.nativeElement.querySelector(".boxC2");
+    const boxC3 = this.elementRef.nativeElement.querySelector(".boxC3");
+
+    tl.timeScale(0.5);
+    tl.to(boxC1, {
+      scale: 0,
+      opacity: 0,
+    });
+    tl.to(boxC2, {
+      x: -50,
+    });
+    tl.to(boxC3, {
+      x: -90,
+      rotate: 360,
+    });
+    tl.set(boxC1, {
+      x: 50,
+    });
+    tl.to(boxC1, {
+      scale: 1,
+      opacity: 1,
+      rotate: 1080,
     });
   }
 }
