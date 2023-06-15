5ac28333c1f566b48555c27cbbdd4c92c94ef344
Gabiel Garcia Diaz <garturogdiaz@gmail.com>
2023-06-15 23:03:48 +0200

    scss udpated

--- src/app/core/side-menu/side-menu.component.ts
+++ src/app/core/side-menu/side-menu.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  templateUrl: './side-menu.component.html'
 })
 export class SideMenuComponent {
 
