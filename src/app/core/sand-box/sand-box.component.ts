5ab0895f2e2e39cf6e1cf621fee445124c5c58b7
gab <garturogdiaz@gmail.com>
2023-06-21 17:29:26 +0200

    neon shapes all

--- src/app/core/sand-box/sand-box.component.ts
+++ src/app/core/sand-box/sand-box.component.ts
@@ -1,12 +1,4 @@
 import { Component, OnInit, ElementRef } from "@angular/core";
-import { gsap } from "gsap";
-
-let tl = gsap.timeline({
-  defaults: {
-    repeat: -1,
-    rotate: 1080,
-  },
-});
 
 @Component({
   selector: "app-sand-box",
@@ -15,28 +7,7 @@ let tl = gsap.timeline({
 export class SandBoxComponent implements OnInit {
   constructor(private elementRef: ElementRef) {}
 
-  ngOnInit() {
-    gsap.set(".neon-square", {});
-    tl = gsap.timeline({ repeat: -1, yoyo: true });
-  }
+  ngOnInit() {}
 
-  ngAfterViewInit() {
-    //   const neonSquare =
-    //     this.elementRef.nativeElement.querySelectorAll(".neon-square");
-    //   tl.to(neonSquare, {
-    //     opacity: 1,
-    //     x: 100,
-    //     y: 150,
-    //     duration: 3,
-    //     ease: "bounce.out",
-    //     stagger: {
-    //       each: 0.2,
-    //       from: "random",
-    //     },
-    //   });
-    //   const navItem = this.elementRef.nativeElement.querySelector(".nav-item");
-    //   navItem.addEventListener("click", () => {
-    //     tl.pause();
-    //   });
-  }
+  ngAfterViewInit() {}
 }
