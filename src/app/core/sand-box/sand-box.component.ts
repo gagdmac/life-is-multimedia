11c6c966b4ef5b63db8a5bdf46af20c1b0bbe630
Gabiel Garcia Diaz <garturogdiaz@gmail.com>
2023-06-23 23:39:26 +0200

    morphong example

--- src/app/core/sand-box/sand-box.component.ts
+++ src/app/core/sand-box/sand-box.component.ts
@@ -11,7 +11,27 @@ export class SandBoxComponent implements OnInit {
   // counter: number = 0;
   // interval: any;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.animateMorphing();
+  }
+
+  animateMorphing() {
+    anime({
+      targets: this.elementRef.nativeElement.querySelectorAll('.morphing-demo .polymorph'),
+      points: [
+        { value: [
+          '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369',
+          '70 41 118.574 59.369 111.145 132.631 60.855 84.631 20.426 60.369']
+        },
+        { value: '70 6 119.574 60.369 100.145 117.631 39.855 117.631 55.426 68.369' },
+        { value: '70 57 136.574 54.369 89.145 100.631 28.855 132.631 38.426 64.369' },
+        { value: '70 24 119.574 60.369 100.145 117.631 50.855 101.631 3.426 54.369' }
+      ],
+      easing: 'easeOutQuad',
+      duration: 2000,
+      loop: true
+    });
+  }
 
   // ngOnInit() {
   //   this.interval = setInterval(() => {
@@ -28,6 +48,9 @@ export class SandBoxComponent implements OnInit {
   // }
 
   ngAfterViewInit() {
+
+    
+
     //test code 10
     const gridElements = Array.from(
       this.elementRef.nativeElement.querySelectorAll(".grid-container div")
